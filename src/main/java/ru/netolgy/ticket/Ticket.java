15b1063e9d67a38233eef13190cd0df4e1cb13ba
Alexandr Timokhin <timohinan79@yandex.ru>
2023-01-20 09:33:53 +0300

    Ticket

---
+++ src/main/java/ru/netolgy/ticket/Ticket.java
@@ -0,0 +1,65 @@
+package ru.netolgy.ticket;
+public class Ticket implements Cloneable <Ticket> {
+    private int id;
+    private String from;
+    private String to;
+    private int price;
+    private int time;
+
+    public Ticket (int id, String from, String to, int price, int time){
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.price = price;
+        this.time = time;
+    }
+
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+
+        this.id = id;
+    }
+
+    public String getFrom(){
+
+        return from;
+    }
+
+    public void setFrom(String from) {
+
+        this.from = from;
+    }
+    public String getTo(){
+
+        return from;
+    }
+
+    public void setTo(String to) {
+
+        this.to = to;
+    }
+
+    public int getPrice() {
+
+        return price;
+    }
+    public void setPrice(int price){
+
+        this.price = price;
+    }
+    public int getTime() {
+
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+
+}
